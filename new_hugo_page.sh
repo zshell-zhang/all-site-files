5cc423702a6421dbae77df715316bd07442e6832
zshell.zhang <zshell.zhang@qunar.com>
2017-01-28 23:39:46 +0800

    backup again

--- new_hugo_page.sh
+++ new_hugo_page.sh
@@ -2,14 +2,17 @@
 
 page_name=$1
 
-if [[ "$page_name" == "" ]]; then
-    echo "\nlack of page name!\n"
-    exit 1
-fi
+# 校验page name合法性
+case $page_name in
+    *.*) echo "page name could not contains \".\"!" 
+	 exit 1 ;;
+     "") echo "\nlack of page name!\n"
+    	 exit 1 ;;
+esac
 
 cd /home/zshell/Documents/zshell.zhang.github.io/
 
-hugo new post/$page_name.md
+#hugo new post/$page_name.md
 
 # 自动进入编辑状态
 vim content/post/$page_name.md
