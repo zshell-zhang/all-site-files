4b20af0b81b7382785c3dbaa90a20caf9f38ffcd
zshell.zhang <xaaservice@gmail.com>
2018-07-05 14:37:18 +0800

    迁移数据至新机器

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
-target_commit_date=$1
+# target_commit_date=$1
 
-random_end_date='2017-12-22'
+# random_end_date='2017-12-22'
 
 
-if [[ $target_commit_date = '' ]]; then
-    sudo date -s $random_end_date
-    let "random_value = $RANDOM % 211"
-    target_commit_date=`date +%Y-%m-%d -d "$random_value days ago"`
-fi
+# if [[ $target_commit_date = '' ]]; then
+#     sudo date -s $random_end_date
+#     let "random_value = $RANDOM % 211"
+#     target_commit_date=`date +%Y-%m-%d -d "$random_value days ago"`
+# fi
 
-echo "target commit date = "$target_commit_date
-sudo date -s $target_commit_date
+# echo "target commit date = "$target_commit_date
+# sudo date -s $target_commit_date
 
-########## 以下为原始内容, commit 均衡后可删除
 echo -e "\nbegin hexo content generating...\n"
 hexo generate
 
 echo -e "\nbegin deploy hexo content to github page...\n"
 hexo deploy
-########## 以上为原始内容, commit 均衡后可删除
-
 
 # 恢复当前时间
-sudo ntpdate 0.asia.pool.ntp.org
+# sudo ntpdate 0.asia.pool.ntp.org
+
